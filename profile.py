32294b2f358ebadf23937752e7f4076f949066d8
Tjaarda1 <abuning@pa.uc3m.es>
2025-12-11 10:56:57 -0800

    upgrade image tag

--- profile.py
+++ profile.py
@@ -16,7 +16,7 @@ import geni.rspec.pg as rspec
 
 BASE_IP = "10.10.1"
 BANDWIDTH = 10000000
-IMAGE = ' urn:publicid:IDN+apt.emulab.net+image+meshbench-PG0:kubeproxy-bm:2'
+IMAGE = ' urn:publicid:IDN+apt.emulab.net+image+meshbench-PG0:kubeproxy-bm:3'
 
 # Set up parameters
 pc = portal.Context()
