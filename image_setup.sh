26f6d0e2aea45b0a65507b57f097055694ff756c
Tjaarda1 <abuning@pa.uc3m.es>
2025-12-16 17:55:58 -0800

    init script: enabled kubectl completion util

--- image_setup.sh
+++ image_setup.sh
@@ -78,6 +78,7 @@ sudo sed -i '/serviceSubnet: .*/a \  podSubnet: "REPLACE_ME_WITH_CIDR"' /etc/kub
 
 sudo sed -i '/name: node/d' /etc/kubeadm/init-config.yaml
 
+echo 'source <(kubectl completion bash)' >>~/.bashrc
 # HELM
 curl -fsSL https://packages.buildkite.com/helm-linux/helm-debian/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/helm.gpg] https://packages.buildkite.com/helm-linux/helm-debian/any/ any main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
