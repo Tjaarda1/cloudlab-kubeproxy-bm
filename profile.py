33c2c55f6fff9f5d213f12842bb66a41d8ae8745
Tjaarda1 <abuning@pa.uc3m.es>
2025-12-18 20:52:02 -0800

    Upgraded disk image version

--- profile.py
+++ profile.py
@@ -16,7 +16,7 @@ import geni.rspec.pg as rspec
 
 BASE_IP = "10.10.1"
 BANDWIDTH = 10000000
-IMAGE = ' urn:publicid:IDN+apt.emulab.net+image+meshbench-PG0:kubeproxy-bm:5'
+IMAGE = ' urn:publicid:IDN+apt.emulab.net+image+meshbench-PG0:kubeproxy-bm:6'
 
 # Set up parameters
 pc = portal.Context()
